03a76986d3537dc00a4941665353929da06361c9
Krishna010804priya <skrishnapriya0108@gmail.com>
2025-03-22 11:16:53 +0530

    add changes for railway

--- server.js
+++ server.js
@@ -1,35 +1,39 @@
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb+srv://Krishnapriya:Harini123@cluster-sympo.ksa7g.mongodb.net/registrationDb");
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("Connected to MongoDB"))
+.catch(err => console.log("MongoDB connection error:", err));
 
-const sampleRegister = {
+const sampleRegisterSchema = new mongoose.Schema({
     title: String,
     content: String
-}
+});
 
-const Sample = mongoose.model("Register", sampleRegister);
+const Sample = mongoose.model("Register", sampleRegisterSchema);
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
-    
-})
+});
 
 app.post("/", function(req, res){
     let newSample = new Sample({
         title: req.body.title,
         content: req.body.content
     });
-    newSample.save();
-    res.redirect('/');
-})
-
+    newSample.save()
+    .then(() => res.redirect("/"))
+    .catch(err => console.log("Error saving data:", err));
+});
 
 app.listen(PORT, function () {
     console.log(`Server is running on port ${PORT}`);
